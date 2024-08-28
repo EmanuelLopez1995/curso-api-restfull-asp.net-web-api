8e163c12ad00895229a179c9be292abc60a1107d
Emanuel Lopez <emanuel.b.lopez@accenture.com>
2024-08-27 22:30:14 -0300

    first commit

---
+++ Proyectos/ApiPeliculas/ApiPeliculas/Modelos/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; } // hay que ponerle Id para que sea incremental
+
+        [Required]
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
